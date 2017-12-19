134c9cb5453976abf43742f9cdd34527a15b293a
DD0s-JCN13 <jimmylin160@gmail.com>
2017-12-19 15:04:27 +0800

    NineLazy Edited

--- src/com/ibm/NineLazy.java
+++ src/com/ibm/NineLazy.java
@@ -6,20 +6,30 @@ public class NineLazy {
 		// TODO Auto-generated method stub
 		for (int i=1;i<=9;i++){
 			for(int j=2;j<=9;j++){
-				if((i*j)<10){
+				if(i>=4&&i<=8){
 					if(j!=9){
-						System.out.print(j+"*"+i+"="+" "+(i*j)+" ");
+						System.out.print("...    ");
 					}
 					else{
-						System.out.println(j+"*"+i+"="+" "+(i*j)+" ");
+						System.out.println("...");
 					}
 				}
 				else{
-					if(j!=9){
-						System.out.print(j+"*"+i+"="+(i*j)+" ");
+					if((i*j)<10){
+						if(j!=9){
+							System.out.print(j+"*"+i+"="+" "+(i*j)+" ");
+						}
+						else{
+							System.out.println(j+"*"+i+"="+" "+(i*j)+" ");
+						}
 					}
 					else{
-						System.out.println(j+"*"+i+"="+(i*j)+" ");
+						if(j!=9){
+							System.out.print(j+"*"+i+"="+(i*j)+" ");
+						}
+						else{
+							System.out.println(j+"*"+i+"="+(i*j)+" ");
+						}
 					}
 				}
 			}
